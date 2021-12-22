75611fa5414150d81ce122b816b9ac87c7a27d87
Glory <75427880+Glory-Dev@users.noreply.github.com>
2021-12-22 20:24:09 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+// Add loading animations with the data storage bar
+let storageBar = document.querySelector(".flyo > .storage > .progress div");
+let storageBarWidth = (815 / 1000) * 100;
+
+window.onload = function () {
+  storageBar.style.width = `81.5%`;
+};
